f0dd0c80e05ac18583dd59b30847a1ff14746ef4
haothetruong257 <haothetruong257@gmail.com>
2025-04-08 21:59:42 +0700

    fix conflict

--- src/services/userServices.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@/types/Users";
-
-export async function getUserById(id: string): Promise<User> {
-  const res = await fetch(`/api/users/${id}`);
-  if (!res.ok) throw new Error("Failed to fetch user");
-  return res.json();
-}
